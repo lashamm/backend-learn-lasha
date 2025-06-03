d5450dad45e173e0a291d0f99d64f7eb56dd2652
lasha <maisuradze.llasha@gmail.com>
2025-06-03 17:07:16 +0400

    first half

--- backend1/Program.cs
+++ backend1/Program.cs
@@ -1,4 +1,77 @@
-﻿// int - integer მთელი რიცხვები
+﻿//                     მეორე გაკვეთილი
+
+
+//Console.WriteLine("enter first num");
+//int firstNumber = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter second num");
+//int secondNumber = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+
+//int sum = firstNumber + secondNumber;
+//int subtraction = firstNumber - secondNumber;
+//int multiplication = firstNumber * secondNumber;
+//int devide = firstNumber / secondNumber;
+//int module = firstNumber % secondNumber;
+
+//Console.Write($"sum of {firstNumber} and {secondNumber} is {sum} " +
+//    $"subtraction of {firstNumber} and {secondNumber} is {subtraction} " +
+//    $"multiplication of {firstNumber} and {secondNumber} is {multiplication} " +
+//    $"{firstNumber} devided by {secondNumber} is {devide} " +
+//    $"{firstNumber}s module of {secondNumber} is {module} ");
+//Console.WriteLine("press anything to exit");
+//Console.Clear();
+
+
+//int x = 10;
+//x += 2; // x = x + 2
+//x -= 2; // x = x - 2
+//x *= 2; // x = x * 2
+//x /= 2; // x = x / 2
+//x %= 2; // x = x % 2
+
+
+//bool b = true && false; // დას(&&) შემთხვევაში თუ ერთერთი false არის მაშინ აბრუნებს false
+//b = true && true; // true
+//b = false && false; // false
+//b = 13 > 0 || 13 < 0; // ანს(||) შემთხვევაში თუ ერთერთი true არის მაშინ აბრუნებს true
+//b = false || false; // false
+//b = true || false; // true
+//b = true || true; // true
+
+
+//char c = 'x';
+//int x = c;
+//c = (char)x;
+
+//string n = "32";
+//int x2 = c;
+//x2 = Convert.ToInt32(n);
+
+Console.WriteLine("enter num");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    Console.WriteLine("num is positive");
+}
+else if (num == 0)
+{
+    Console.WriteLine("num is 0");
+}
+else
+{
+    Console.WriteLine("num is negative");
+}
+
+
+
+
+
+
+
+//                     პირველი გაკვეთილი
+
+
+// int - integer მთელი რიცხვები
 // double - ათწილადები
 // string - სტრინგები
 // char - ერთი სიმბოლო
@@ -13,17 +86,18 @@
 //bool n = false; ././
 
 
-string name;
-string lastName;
-int age;
+//string name;
+//string lastName;
+//int age;
 
-Console.WriteLine("enter your name");
-Console.WriteLine("enter your lastname");
-Console.WriteLine("enter your age");
+//Console.WriteLine("enter your name");
+//Console.WriteLine("enter your lastname");
+//Console.WriteLine("enter your age");
 
-name = Console.ReadLine();
-lastName = Console.ReadLine();
-age = Convert.ToInt32(Console.ReadLine());
+//name = Console.ReadLine();
+//lastName = Console.ReadLine();
+//age = Convert.ToInt32(Console.ReadLine());
 
 
-Console.WriteLine($"hello {name} your lastname is {lastName} and you are {age} years old");
+//Console.WriteLine($"hello {name} your lastname is {lastName} and you are {age} years old");
+
