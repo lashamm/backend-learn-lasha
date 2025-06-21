e05fdd51d2cbf46e9d89c11fbdf03efc5210aa90
lasha <maisuradze.llasha@gmail.com>
2025-06-21 17:50:55 +0400

    done

---
+++ backend1/User.cs
@@ -0,0 +1,43 @@
+﻿namespace backend1
+{
+    internal class Users
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; private set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Users() { }
+        public Users(
+            string name,
+            string lastName,
+            string email,
+            DateTime birthDate
+            )
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public int getAge()
+        {
+            int result = DateTime.Now.Year - BirthDate.Year;
+            if (DateTime.Now.DayOfYear < BirthDate.DayOfYear)
+            {
+                result--;
+            }
+            return result;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}, " +
+                $"LastName: {LastName}, " +
+                $"Mail: {Email}, " +
+                $"Age: {getAge()}.");
+        }
+    }
+
+}
